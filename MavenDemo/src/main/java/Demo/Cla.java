d3e05d292b009d6356a723335b008d2e26209a76
user <user@qaplanet35>
2017-11-21 13:05:09 +0530

    ADD Test

---
+++ MavenDemo/src/main/java/Demo/Cla.java
@@ -0,0 +1,20 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class Cla 
+{
+@Test
+public void method1()
+{
+	System.out.println("arun");
+}@Test
+public void method2()
+{
+	System.out.println("ramki");
+}@Test
+public void method3()
+{
+	System.out.println("ram");
+}
+}
